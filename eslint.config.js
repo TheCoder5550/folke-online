c19fe62ba0c594d796233c2b5bf9b2187ca0c103
TheCoder5550 <alfons.nilsson@outlook.com>
2025-07-15 15:47:58 +0000

    Add ts-reset + more linting

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,14 @@ export default tseslint.config([
     },
 
     rules: {
+      "@typescript-eslint/no-unsafe-argument": "error",
+      "@typescript-eslint/no-unsafe-assignment": "error",
+      "@typescript-eslint/no-unsafe-call": "error",
+      "@typescript-eslint/no-unsafe-member-access": "error",
+      "@typescript-eslint/no-unsafe-return": "error",
       "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unsafe-function-type": "error",
+
       "@typescript-eslint/no-unused-vars": "warn",
     }
   },
