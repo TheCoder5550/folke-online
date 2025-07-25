b3c2b1ed3e02b47728d12b0642c7c3f2b67804f6
TheCoder5550 <alfons.nilsson@outlook.com>
2025-07-25 20:34:48 +0000

    Added exercise support using Markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import './App.css'
 import ErrorBoundary from './components/ErrorBoundary'
+import Exercise from './components/Exercise'
 import PracticeProofRenderer from './components/PracticeProofRenderer'
 import ProofRenderer from './components/ProofRenderer'
 import { createExercise } from './helpers/proof-helper'
 import { ProofStoreProvider } from './stores/proof-store'
+
+const markdownFileContent = (await import(`./exercises/exercise-1.md?raw`)).default;
 
 function App() {
   return (
     <>
       <ErrorBoundary>
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <Exercise markdown={markdownFileContent} />
+
           {/* <ProofStoreProvider localStorageName='current-proof-storage'>
             <ProofRenderer />
           </ProofStoreProvider>
 
           <ProofStoreProvider initialProof={createExercise(["A", "B"], "A & B")} localStorageName='exercise-1'>
             <PracticeProofRenderer />
-          </ProofStoreProvider> */}
+          </ProofStoreProvider>
 
           <ProofStoreProvider initialProof={createExercise(["P -> !P"], "!P")} localStorageName='exercise-2'>
             <PracticeProofRenderer />
-          </ProofStoreProvider>
+          </ProofStoreProvider> */}
         </div>
       </ErrorBoundary>
     </>
---
+++ src/components/Exercise.tsx
@@ -0,0 +1,88 @@
+import PracticeProofRenderer from "./PracticeProofRenderer";
+import { ProofStoreProvider } from "../stores/proof-store";
+import { createExercise } from "../helpers/proof-helper";
+import Markdown from "markdown-to-jsx";
+
+interface ExerciseProps {
+  markdown: string;
+}
+
+export default function Exercise(props: ExerciseProps) {
+  // const proofExt: TokenizerAndRendererExtension<JSX.Element, JSX.Element> = {
+  //   name: 'proof',
+  //   level: 'block',                                     // Is this a block-level or inline-level tokenizer?
+  //   start(src) { return src.match(/!p/)?.index; }, // Hint to Marked.js to stop and check for a match
+  //   tokenizer(src, tokens) {
+  //     const str = src;
+  //     const regex = /^!proof(?:\[(.+?)=(.+?)\])(?:\[(.+?)=(.+?)\])?/;
+  //     const match = str.match(regex);
+  //     if (match) {
+  //       let sequent = "";
+  //       if (match[1] === "sequent") {
+  //         sequent = match[2];
+  //       }
+  //       else if (match[3] === "sequent") {
+  //         sequent = match[4];
+  //       }
+
+  //       const split = sequent.split("=>");
+  //       const premises = split[0].split(";").map(p => p.trim());
+  //       const conclusion = split[1].trim();
+
+  //       const token = {                                 // Token to generate
+  //         type: 'proof',                                // Should match "name" above
+  //         raw: match[0],                                // Text to consume from the source
+  //         premises,
+  //         conclusion
+  //       };
+  //       return token;
+  //     }
+  //   },
+  //   renderer(token) {
+  //     return (
+  //       <ProofStoreProvider initialProof={createExercise(token.premises, token.conclusion)} localStorageName='exercise-md'>
+  //         <PracticeProofRenderer />
+  //       </ProofStoreProvider>
+  //     )
+  //   }
+  // };
+
+  // marked.use({ extensions: [proofExt] });
+  // const parsed = marked.parse(props.markdown);
+
+  return (
+    <div style={{ width: "700px" }}>
+      <Markdown
+        children={props.markdown}
+        options={{
+          overrides: {
+            Proof: {
+              component: ProofComp,
+            },
+          },
+        }}
+      />
+    </div>
+  )
+}
+
+interface ProofCompProps {
+  sequent?: string;
+  solution?: string;
+}
+
+function ProofComp(props: ProofCompProps) {
+  if (props.sequent == undefined) {
+    return <span>No sequent provided</span>
+  }
+
+  const split = props.sequent.split("|-");
+  const premises = split[0].split(";").map(p => p.trim());
+  const conclusion = split[1].trim();
+
+  return (
+    <ProofStoreProvider initialProof={createExercise(premises, conclusion)} localStorageName='exercise-md'>
+      <PracticeProofRenderer />
+    </ProofStoreProvider>
+  )
+}
