bb820b8ed54ed2b8b05d683847a14ad1a4164427
TheCoder5550 <alfons.nilsson@outlook.com>
2025-07-28 16:28:11 +0000

    Updated exercises page

--- src/App-exercises.tsx
+++ src/App-exercises.tsx
@@ -1,23 +1,37 @@
+import { useState } from 'react';
 import ErrorBoundary from './components/ErrorBoundary';
 import ExamList from './components/ExamList';
 import Exercise from './components/Exercise'
 import Header from './components/Header';
+import { COMPONENT_LIST } from './exercise-components/AllExams';
 
 const exercise1 = (await import(`./assets/exercises-markdown/exercise-1.md?raw`)).default;
 const exercise2 = (await import(`./assets/exercises-markdown/exercise-2.md?raw`)).default;
 
 function App() {
+  const [index, setIndex] = useState<number | null>(null);
+
   return (
     <>
       <ErrorBoundary>
         <Header />
 
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "1rem", paddingBottom: "1rem" }}>
-          <div style={{ padding: "3rem 6rem", border: "1px solid gray", background: "white", width: "100%", maxWidth: "1000px" }}>
-            <ExamList />
-            
-            {/* <Exercise markdown={exercise1} name="1" key="1" />
-            <Exercise markdown={exercise2} name="2" key="2" /> */}
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <ExamList index={index} setIndex={setIndex} />
+          
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "1rem", paddingBottom: "1rem", flexGrow: "1" }}>
+            <div style={{ padding: "3rem 6rem", border: "1px solid gray", background: "white", width: "100%", maxWidth: "1000px", flexGrow: "1" }}>
+              <h1>Exam Question</h1>
+
+              {index != null ? (
+                COMPONENT_LIST[index]()
+              ) : (
+                <span>Select a question</span>
+              )}
+
+              {/* <Exercise markdown={exercise1} name="1" key="1" />
+              <Exercise markdown={exercise2} name="2" key="2" /> */}
+            </div>
           </div>
         </div>
       </ErrorBoundary>
--- src/components/ExamList.tsx
+++ src/components/ExamList.tsx
@@ -2,20 +2,30 @@ import { useState } from "react";
 import { COMPONENT_LIST } from "../exercise-components/AllExams"
 import { EXAM_NAMES} from "../exercise-components/exam-data.ts";
 
-export default function ExamList() {
-  const [index, setIndex] = useState<number | null>(null);
+interface ExamListProps {
+  index: number | null;
+  setIndex: React.Dispatch<React.SetStateAction<number | null>>
+}
 
+export default function ExamList(props: ExamListProps) {
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem"}}>
-      <h1>All Exam Questions</h1>
-
-      {index != null && (
-        COMPONENT_LIST[index]()
-      )}
-
-      <h2>Index</h2>
-      {EXAM_NAMES.map((exam, index) => (
-        <a key={exam} href="#" onClick={() => setIndex(index)}>{exam}</a>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      gap: "0.5rem",
+      overflow: "auto",
+      height: "90vh",
+      background: "white",
+      border: "1px solid black",
+      padding: "1rem",
+      margin: "1rem",
+      position: "sticky",
+      top: "1rem",
+      borderRadius: "4px"
+    }}>
+      <h2>All exam questions</h2>
+      {EXAM_NAMES.map((exam, i) => (
+        <a key={exam} href="#" onClick={() => props.setIndex(i)} style={{ color: props.index === i ? "red" : undefined }}>{exam}</a>
       ))}
     </div>
   )
