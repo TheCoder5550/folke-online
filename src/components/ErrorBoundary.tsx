0dd966f5adb71d716a4d58e6059e79dc120678cc
TheCoder5550 <alfons.nilsson@outlook.com>
2025-10-10 09:35:51 +0000

    Added clear localStorage button to error page

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -30,6 +30,14 @@ class ErrorBoundary extends Component<Props, State> {
         }}>
           <h1>Sorry, there was an error.</h1>
           <p>Try refreshing the page</p>
+          <p>If that doesn't work, try clearing cookies and page data</p>
+          <button
+            type="button"
+            className="action-button danger"
+            onClick={clearData}
+          >
+            Clear page data
+          </button>
         </div>
       );
     }
@@ -39,3 +47,12 @@ class ErrorBoundary extends Component<Props, State> {
 }
 
 export default ErrorBoundary;
+
+function clearData() {
+  if (!confirm("Are you sure you want to clear the data? All exercise progress and created proofs will be lost!")) {
+    return;
+  }
+  
+  localStorage.clear();
+  window.location.reload();
+}
