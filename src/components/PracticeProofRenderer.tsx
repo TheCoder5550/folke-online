889e60699a755ade550106bacccaa557bc85ff96
TheCoder5550 <alfons.nilsson@outlook.com>
2025-10-11 18:23:45 +0000

    Fixed undo/redo keybinds when there are multiple proofs

--- src/components/PracticeProofRenderer.tsx
+++ src/components/PracticeProofRenderer.tsx
@@ -6,13 +6,13 @@ import useProofStore, { ProofDispatchActionTypeEnum } from "../stores/proof-stor
 import ValidateButton from "./ValidateButton/ValidateButton";
 import { LuListRestart } from "react-icons/lu";
 import GlobalErrorMessage from "./GlobalErrorMessage";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ImRedo, ImUndo } from "react-icons/im";
 import StaticProofRenderer from "./StaticProofRenderer/StaticProofRenderer";
 import { FaLightbulb } from "react-icons/fa6";
 import Modal from "./Modal/Modal";
 import { isKeybindPressed, showKeybindName } from "../helpers/keybinds";
-import { cls } from "../helpers/generic-helper";
+import { cls, findParentElement } from "../helpers/generic-helper";
 import { getSequent } from "../helpers/proof-helper";
 import { useScreenSize } from "../helpers/use-screen-size";
 import LastRowActions from "./LastRowActions";
@@ -24,35 +24,69 @@ interface PracticeProofRendererProps {
 
 export default function PracticeProofRenderer(props: PracticeProofRendererProps) {
   const isMobile = useScreenSize() === "mobile";
+
   const [showSolution, setShowSolution] = useState(false);
+  const [thisProofHasFocus, setThisProofHasFocus] = useState(false);
+
+  const undo = useProofStore((state) => state.undo);
+  const redo = useProofStore((state) => state.redo);
   const dispatch = useProofStore((state) => state.dispatch);
   const sequent = useProofStore((state) => getSequent(state.getProof()));
+  const stepLookup = useProofStore((state) => state.getProof().stepLookup);
 
-  const maybeShowSolution = () => {
+  const maybeShowSolution = useCallback(() => {
     if (confirm("Are you sure you want to spoil the solution?")) {
       setShowSolution(true)
     }
-  }
+  }, [ setShowSolution ]);
 
-  const startOver = () => {
+  const startOver = useCallback(() => {
     if (confirm("Are you sure you want to start over? All progress will be deleted!")) {
       dispatch({
         type: ProofDispatchActionTypeEnum.Retry
       })
     }
-  };
+  }, [ dispatch ]);
 
-  const undo = useProofStore((state) => state.undo);
-  const redo = useProofStore((state) => state.redo);
+  // Check if the last focused input belongs to this proof
+  useEffect(() => {
+    const onFocus = (e: FocusEvent) => {
+      if (!(e.target instanceof HTMLElement)) {
+        return;
+      }
 
+      const row = findParentElement(e.target, (element) => element.hasAttribute("data-uuid"));
+      if (!row) {
+        return;
+      }
+
+      const uuid = row.getAttribute("data-uuid");
+      if (uuid == null) {
+        return;
+      }
+
+      setThisProofHasFocus(uuid in stepLookup);
+    };
+
+    window.addEventListener("focus", onFocus, true);
+    return () => {
+      window.removeEventListener("focus", onFocus, true);
+    }
+  }, [ stepLookup ]);
+
+  // Undo/redo if this proof has focus
   useEffect(() => {
     const keydown = (e: KeyboardEvent) => {
       if (isKeybindPressed("undo", e)) {
-        undo();
+        if (thisProofHasFocus) {
+          undo();
+        }
         e.preventDefault();
       }
       if (isKeybindPressed("redo", e)) {
-        redo();
+        if (thisProofHasFocus) {
+          redo();
+        }
         e.preventDefault();
       }
     };
@@ -61,7 +95,7 @@ export default function PracticeProofRenderer(props: PracticeProofRendererProps)
     return () => {
       window.removeEventListener("keydown", keydown);
     }
-  }, [ undo, redo ]);
+  }, [ undo, redo, stepLookup, thisProofHasFocus ]);
 
   return (
     <StepsContainer>
